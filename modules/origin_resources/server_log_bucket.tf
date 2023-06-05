1c07ae1f47ec0135d3e7397d7288d0b7037b9cd6
Igor <igorr@creditguard.co.il>
2023-06-05 15:09:11 +0300

    Remove bucket ACL that allows sending logs to log_bucket from source bucket, use policy instead

--- modules/origin_resources/server_log_bucket.tf
+++ modules/origin_resources/server_log_bucket.tf
@@ -2,12 +2,53 @@ resource "aws_s3_bucket" "log_bucket" {
   bucket = var.log_bucket_name
 }
 
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
+
+resource "aws_s3_bucket_policy" "allow_logging_from_another_bucket" {
   bucket = aws_s3_bucket.log_bucket.id
-  acl    = var.log_bucket_acl
+  policy = data.aws_iam_policy_document.allow_logging_from_another_bucket.json
 }
 
-resource "aws_s3_bucket_logging" "example" {
+data "aws_iam_policy_document" "allow_logging_from_another_bucket" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["logging.s3.amazonaws.com"]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+
+      values = [
+        "arn:aws:s3:::${var.source_bucket}"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+
+      values = [
+        var.aws_account_id
+      ]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.log_bucket.name}/*"
+    ]
+  }
+}
+
+# resource "aws_s3_bucket_acl" "log_bucket_acl" {
+#   bucket = aws_s3_bucket.log_bucket.id
+#   acl    = var.log_bucket_acl
+# }
+
+resource "aws_s3_bucket_logging" "source_bucket_logging" {
   bucket = var.source_bucket_id
 
   target_bucket = aws_s3_bucket.log_bucket.id
